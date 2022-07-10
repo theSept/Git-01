b54bd628f73e7828889849a421813f2c44cb09fc
xu <3232129642@qq.com>
2022-07-10 20:45:33 +0800

    tow idea commit

--- src/main/java/com/xu/git/TestGit.java
+++ src/main/java/com/xu/git/TestGit.java
@@ -9,5 +9,6 @@ package com.xu.git;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hello ,Git!");
+        System.out.println("Hello ,Git2!");
     }
 }
