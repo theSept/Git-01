f013c1ef561922aaefc788a396602a8f699c7838
xu <3232129642@qq.com>
2022-07-10 20:48:07 +0800

    three idea commit

--- src/main/java/com/xu/git/TestGit.java
+++ src/main/java/com/xu/git/TestGit.java
@@ -10,5 +10,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hello ,Git!");
         System.out.println("Hello ,Git2!");
+        System.out.println("Hello ,Git3!");
     }
 }
