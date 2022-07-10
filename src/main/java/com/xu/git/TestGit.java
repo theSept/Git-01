71877b36c324c77671205c8cbead58d8bf000f30
xu <3232129642@qq.com>
2022-07-10 20:42:10 +0800

    first idea commit

---
+++ src/main/java/com/xu/git/TestGit.java
@@ -0,0 +1,13 @@
+package com.xu.git;
+
+/**
+ * User: 彼暗
+ * Date: 2022-07-10
+ * Time: 20:37
+ * Versions: 1.0.0
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("Hello ,Git!");
+    }
+}
